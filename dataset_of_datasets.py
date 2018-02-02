1fb6d4b02bc27385dce095b5ecaf8626bf0f4668
supreshc-persistent <supresh_colaco@persistent.com>
2018-02-02 17:51:34 +0530

    minor changes and error handling

--- dataset_of_datasets.py
+++ dataset_of_datasets.py
@@ -3,28 +3,40 @@ import urllib2
 import urllib
 import json
 import requests
+import sys
+
+ckan_base = 'http://127.0.0.1:5000'
+ckan_api_key = '5d9d0f6c-cab6-4704-9c03-3713044d5770'
+package_id = 'dataset-of-datasets'
 
 #function to get all the site package lists and its resources
 def get_packages_data():
     response=[]
-    #r = requests.get('http://127.0.0.1:5000/api/3/action/current_package_list_with_resources',headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
-    package_list = requests.get('http://127.0.0.1:5000/api/3/action/package_list',headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
+    package_list = requests.get(ckan_base + '/api/3/action/package_list',headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
     package_list = package_list.json()['result']
 
     for package in package_list:
-        if package == 'dataset-of-datasets':
+        if package == package_id:
             continue
-        post_dict = {'id':package}
-        post_dict  = urllib.quote(json.dumps(post_dict))
-        r = requests.post('http://127.0.0.1:5000/api/3/action/package_show',data=post_dict,headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
-        response.append(r.json()['result'])
+        try:
+            post_dict = {'id':package}
+            post_dict  = urllib.quote(json.dumps(post_dict))
+            r = requests.post(ckan_base + '/api/3/action/package_show',data=post_dict,headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
+            response.append(r.json()['result'])
+        except:
+            print "Error fetching package data for "+package
 
     return response
 
 #function to get the schema dataset fields
 def get_dataset_fields():
-    r = requests.get('http://127.0.0.1:5000/api/3/action/scheming_dataset_schema_show?type=dataset',headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
-    schema_dataset_fields = r.json()['result']['dataset_fields']
+    schema_dataset_fields = []
+    try:
+        r = requests.get(ckan_base + '/api/3/action/scheming_dataset_schema_show?type=dataset',headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
+        schema_dataset_fields = r.json()['result']['dataset_fields']
+    except:
+        print "Error fetching dataset schema fields"
+
     dataset_fields = {}
     for schema_dataset_field in schema_dataset_fields:
         dataset_fields[schema_dataset_field['field_name']] = schema_dataset_field['label']
@@ -38,19 +50,19 @@ def get_package_tags(tags_dict):
     tags = [tag.get('display_name') for tag in tags_dict]
     return ",".join(tags)
 
-ckan_api_key = '5d9d0f6c-cab6-4704-9c03-3713044d5770'
+#get the dataset to which resource is to be created/updated
+try:
+    post_dict = {'id':package_id}
+    post_dict  = urllib.quote(json.dumps(post_dict))
+    r = requests.post(ckan_base + '/api/3/action/package_show',data=post_dict,headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
+    package_data = r.json()['result']
+    resources = package_data['resources']
+except:
+    print "Error fetching package data for "+package_id
+    sys.exit()
 
 
 dataset_fields = get_dataset_fields()
-
-#get the dataset to which resource is to be created/updated
-post_dict = {'id':'dataset-of-datasets'}
-post_dict  = urllib.quote(json.dumps(post_dict))
-r = requests.post('http://127.0.0.1:5000/api/3/action/package_show',data=post_dict,headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
-
-package_data = r.json()['result']
-resources = package_data['resources']
-
 #if dataset has no exisiting resource then create a resource and add records
 if len(resources) == 0:
     #create a resource and add data to the created resource
@@ -74,7 +86,7 @@ if len(resources) == 0:
         records.append(record_data)
     post_dict = {'resource':{'package_id':'dataset-of-datasets','name':'Datasets List','resource_type':'csv'},'records':records,'primary_key':['URL']}
     post_dict  = urllib.quote(json.dumps(post_dict))
-    r = requests.post('http://127.0.0.1:5000/api/3/action/datastore_create',data=post_dict,headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
+    r = requests.post(ckan_base + '/api/3/action/datastore_create',data=post_dict,headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
     result = r.json()
 else:
     res_id = resources[0]['id']
@@ -101,6 +113,5 @@ else:
 
     post_dict = {'resource_id':res_id,'records':records}
     post_dict  = urllib.quote(json.dumps(post_dict))
-    r = requests.post('http://127.0.0.1:5000/api/3/action/datastore_upsert',data=post_dict,headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
+    r = requests.post(ckan_base + '/api/3/action/datastore_upsert',data=post_dict,headers={'X-CKAN-API-Key':ckan_api_key,'Content-type':'application/x-www-form-urlencoded'})
     result = r.json()
-    #print result
